96bf74f6f7bd04bf9885bbc86effadbb454a0f9c
Demetris Chrysostomou <demetrischrysostomou1@gmail.com>
2021-04-06 22:59:33 +0300

    Initial Files Commit
    Prewritten files that I had in an online IDE

---
+++ data_structures.py
@@ -0,0 +1,92 @@
+print("***Read important theory comments at beginning of code***\n")
+# data structure theory: mutability and order
+# an object is mutable if it can be modified after it is created (string are immutable)
+# an object is ordered if the order of its elements matter and this is used to be able to access its elements based on indexing/order (strings are ordered)
+
+# lists follow
+print("-> Lists:\n\n")
+
+# lists can store a lot of values in one variable (even of different data types)
+
+# lists are mutable and ordered objects
+
+# list to be used in following examples
+data = ['loizos',True,64.4,'GET rekt',360]
+print("Sample list:", data)
+
+# positive indexing starts from the first element at index 0
+print("Element at index 2 is:", data[2])
+print("Element at index 0 is:", data[0])
+
+# negative indexing starts from the last element at index = -1
+print("Element at index -1 is:", data[-1])
+print("Element at index -5 is:", data[-5])
+
+# slicing list[l:r] is a substring indexing which returns a list of all elements from index 'l' to 'r-1' (lower bound is inclusive, but upper is not)
+print("\nElements sliced [1:3]:", data[1:3])
+print("Elements sliced [:2]", data[:2])
+print("Elements sliced [-2:]", data[-2:])
+
+# 'in' and 'not in' operators are used to identify whether an object is included in another object (the list in this case)
+print("\nIs element 'True' in?", True in data)
+print("Is element 'False' in?", False in data)
+print("Is element '64.40' in?", 64.40 in data)
+print("Is element '\"george\"' not in?", "george" not in data)
+
+# when mutable objects are copied both variables reference the same object
+data_copy = data
+print("\nThe 2 lists after copying are:")
+print(data)
+print(data_copy)
+
+# modifying the copy of the list
+data_copy[2] = 3
+print("\nAfter modifying the list copy both lists change:")
+print(data)
+print(data_copy)
+
+# useful functions follow (also used in lists)
+sample_list = [2,132,5,-2,68]
+print("\n\nThe sample list to be used in the functions that follow is:", sample_list)
+
+# len() function was mentioned at fundamentals
+print("\nThe length of the list is:", len(sample_list))
+
+# max() function returns the greatest element of the input
+print("\nThe greatest element of the list is:", max(sample_list))
+# !!! be careful when using functions that compare elements -> mix of data types will produce an error
+
+# min() function returns the smallest element of the input
+print("\nThe smallest element of the list is:", min(sample_list))
+
+# sorted() function returns a copy of the input after being sorted
+# can also define the optional argument {reverse=True} for descending sorted
+print("\nThe list sorted in ascending order is:", sorted(sample_list))
+print("The list sorted in descedning order is:", sorted(sample_list,reverse=True))
+
+# useful methods follow (also used in lists)
+sample_list = ["George","Milky way","Neutron","Sun"]
+print("\n\nThe sample list to be used in the methods that follow is:", sample_list)
+
+# join() method joins the elements of the input list each separated by the string specified
+# the return value is a string
+print("\nThe list joined together and separated by '-' is:", "-".join(sample_list))
+print("The list joined together and separated by '\\n' is:\n" + "\n".join(sample_list))
+# !!! be careful as this method will only work if elements of list are all strings
+
+# append() method inserts element inputed at the end of the list
+sample_list.append("Star")
+print("\nThe list after appending '\"Star\"' is:", sample_list)
+
+# insert() method inserts element inputed at the index specified shifting elements to the right
+sample_list.insert(3,True)
+print("\nThe list after inserting 'True' at index 3 is:", sample_list)
+
+# remove() method removes element inputed from list
+sample_list.remove("Sun")
+print("\nThe list after removing '\"Sun\"' is:", sample_list)
+# !!! be careful when removing data from lists -> must exist (care CAPS)
+# names.remove("sun") will produce an error because of lowercase 's'
+
+
+
---
+++ fundamentals.py
@@ -0,0 +1,91 @@
+# print() function outputs on the screen the inputted string
+print("Welcome to my first python program\n")
+
+# input() function prompts the user to enter a value
+f_name = input("Please enter your first name:\n")
+l_name = input("Please enter your last name:\n")
+
+# outputting a hello message with the full name of the user
+print("\nHello", f_name, l_name, "!\n")
+
+# variables used to store data about a movie with different data types
+# variable names in python are usually written in snake_form (Lcase with underscores)
+movie_name = "world war z"
+rating = 9.2
+times_viewed = 60
+favourite = False
+# Python is case-sensitive -> false is not recognised, but False is
+
+# type() function returns the data type of the input
+print("The data type of value 'False' is:", type(False))
+print("The data type of value '192.' is:", type(192.))
+
+# {data_type}() function casts the input into the specified data type (eg.str(x))
+print("\nThe input '\"2\"' when casted into a float is:", float("2"))
+print("The input '25.3' when casted into a boolean is:", bool(25.3))
+print("The input '3.6' when casted into an integer is:", int(3.63))
+
+# round() function rounds input to decimal places specified (negative d.p. are valid)
+print("\nThe input '122.4516' when rounded down to -1 d.p. is:", round(122.4516,-1))
+print("The input '122.4516' when rounded down to 0 d.p. is:", round(122.4516))
+print("The input '122.4516' when rounded down to 3 d.p. is:", round(122.4516,3))
+
+# len() function returns the size of the input
+print("\nThe length of the input 'lesgo G' is:", len('lesgo G'))
+# inputting a number will return an error
+
+# strings will follow
+print("\n\nStrings follow\n")
+
+# use backslash before single quotes if using both types of quote in a string for correct syntax
+quotes_in_string = '"You\'re very talented, he insisted."'
+print("\nString with both types of quotes follows:", quotes_in_string)
+
+# plus sign used to concatenate strings
+quotes_in_string += ' I was amazed!'
+print("\nConcatenated string follows:", quotes_in_string)
+print("The new string is of lenght:", len(quotes_in_string))
+
+# multiplication sign used to repeat strings
+quotes_in_string *= 4
+print("\nRepeating string using '*' follows:", quotes_in_string)
+
+# string methods follow
+sample_string = "Dremixis, is an OMNIGOD."
+print("\n\nThe sample string used in the following method examples is:", sample_string)
+
+# title() method changes all first letters to uppercase
+print("\nThe title() method converts the string to:", sample_string.title())
+
+# islower() method checks whether all characters are lowercase
+print("\nThe islower() method returns:", sample_string.islower())
+
+# count() method counts occurrences of a specified string in the sample_string
+print("\nThe count('is') method returns:", sample_string.count('is'))
+
+# format() method can replace {} in a string with the specified input
+# this method can be really useful in print() statements
+print("\n\nThe format() method can modify string output using {}, see code examples")
+
+print("\nThe player named {} always beats G2 in {}v1 situations".format("Faker",3))
+
+counter_champion = "Volibear"
+print("\nWhen I pick Darius the enemies counterpick me with {}".format(counter_champion))
+
+# split() method splits string into words separated by a specified separator and returns it as a list of strings
+# split(sep, maxsplits) are the possible arguments which are self-explanatory
+print("\n\nThe default split() method returns:", sample_string.split())
+
+print("\nWhen the string is separated by ',' we get:", sample_string.split(","))
+
+print("\nWhen the method is split(' ',2) it returns:", sample_string.split(" ",2))
+
+print("\nWhen the method is split(None,7) it returns:", sample_string.split(None,7))
+
+# find() method returns the index of the first occurrence of the specified sample_string
+print("\n\nThe first occurence of 'is' in the string starts at index:", sample_string.find("is"))
+
+# rfind() method works in the same way but searches for the last occurrence
+print("\nThe last occurrence of 'is' in te string starts at index:", sample_string.rfind("is"))
+
+
